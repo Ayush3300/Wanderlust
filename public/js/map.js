5c41313ee167e3b2fe831a040e7471578a34a0ae
Ayush3300 <ayushmutha6767@gmail.com>
2026-02-16 14:58:47 +0530

    fix:syntax error

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,17 @@
 maptilersdk.config.apiKey = mapToken;
+const listing = JSON.parse(
+  document.getElementById("listing-data").textContent
+);
 const map = new maptilersdk.Map({
-  container: "map", // container's id or the HTML element to render the map
+  container: "map",
   style: maptilersdk.MapStyle.STREETS,
-  center: listing.geometry.coordinates, // [lng, lat]
+  center: listing.geometry.coordinates,
   zoom: 12,
 });
-
-// console.log(coordinates)
-const marker = new maptilersdk.Marker({ color: "red" })
-    .setLngLat(listing.geometry.coordinates)
-    .setPopup(
+new maptilersdk.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
     new maptilersdk.Popup({ offset: 25 })
-      .setHTML(`<h4>${listing.title}</h4> <p>Exact Location will be shared after booking</p>`) // You can pass listing title from EJS too
+      .setHTML(`<h4>${listing.title}</h4><p>Exact Location will be shared after booking</p>`)
   )
-    .addTo(map);
+  .addTo(map);
